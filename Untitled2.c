e70199e430e8d1dfafe761cfd5e74f7950bc68da
Kavishka231 <mr.kavishkasandeepa@gmail.com>
2026-02-02 14:16:31 +0530

    Add extra file to the repo

---
+++ Untitled2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+(
+ float num1,num2,total;
+ 
+ printf("Enter first number:",num1);
+ scanf("%d",num1);
+ 
+ printf("Enter first number:",num2);
+ scanf("%d",num2);
+ 	
+ 	total=num1+num2;
+ 	
+ 	return 0;
